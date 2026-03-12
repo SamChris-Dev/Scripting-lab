efd36b4c6eec76b148c1246ce6c3303f7260f0d2
Mateuszzz <s097122@student.tu.kielce.pl>
2026-03-12 09:32:57 +0100

    second

--- pe1-module3/lab_3_2_4_guess_number.py
+++ pe1-module3/lab_3_2_4_guess_number.py
@@ -10,3 +10,11 @@ print(
 | So, what is the secret number? |
 +================================+
 """)
+
+while secret_number:
+    num = int(input("Enter an integer: "))
+    if secret_number == num:
+     print(secret_number, "Well done, muggle! You are free now.")
+    else:
+     print("Ha ha! You are stuck in my loop!")
+
