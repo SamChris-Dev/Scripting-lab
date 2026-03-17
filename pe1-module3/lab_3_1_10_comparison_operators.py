0dc064ad7fe0a5a86673abab806596232404f568
Samwel Simule <s098153@student.tu.kielce.pl>
2026-03-17 20:26:21 +0100

    Modified lab 3.1.10 task

--- pe1-module3/lab_3_1_10_comparison_operators.py
+++ pe1-module3/lab_3_1_10_comparison_operators.py
@@ -1,6 +1,6 @@
 # Simple program that prints the sentence "Yes - Spathiphyllum is the best plant ever!" to the screen if the inputted string is "Spathiphyllum"
 # prints "No, I want a big Spathiphyllum!" if the inputted string is "spathiphyllum" (lower-case)
-#  prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.
+# prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.
 
 
 String = input("Please enter your string: ")
