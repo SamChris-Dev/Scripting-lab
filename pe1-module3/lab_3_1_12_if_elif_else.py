21cfa7837cd9c9306e244c5e4456a197df5e8476
Samwel Simule <s098153@student.tu.kielce.pl>
2026-03-18 00:10:16 +0100

    Completed lab 3.1.12 task

---
+++ pe1-module3/lab_3_1_12_if_elif_else.py
@@ -0,0 +1,18 @@
+# A simple program that determines if the year is leap or common.
+# According to the Gregorian Calendar
+
+
+year = int(input("Enter a year: "))
+
+if year < 1582:
+	print("Not within the Gregorian calendar period")
+else:
+    #  Write the if-elif-elif-else block here.
+	if year%4 != 0:
+		print("It's a common year")
+	elif year%100 != 0:
+		print("It's a leap year")
+	elif year%400 != 0:
+		print("It's a common year")
+	else:
+		print("It's a leap year")
