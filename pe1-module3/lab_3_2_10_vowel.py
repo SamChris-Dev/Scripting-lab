57210f923f5f37114569b543f8dc7c6f2ee5f08f
Samwel Simule <s098153@student.tu.kielce.pl>
2026-03-31 01:07:17 +0200

    Implement vowel eater program to filter out vowels from user input

---
+++ pe1-module3/lab_3_2_10_vowel.py
@@ -0,0 +1,18 @@
+# The vowel eater program
+user_word = input("Enter your word: ")
+
+user_word = user_word.upper()
+
+for i in user_word:
+    if i == "A":
+        continue
+    elif i == "E":
+        continue
+    elif i == "I":
+        continue
+    elif i == "O":
+        continue
+    elif i == "U":
+        continue
+    else:
+        print(i)
