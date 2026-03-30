c39a8307b1494fb592cb9eae6873a1d87a655cab
Samwel Simule <s098153@student.tu.kielce.pl>
2026-03-31 00:42:11 +0200

    Add new lab exercises for loops and input handling

---
+++ pe1-module3/lab_3_2_9_stuck_loop.py
@@ -0,0 +1,7 @@
+# string = input("Enter a word: ")
+
+while True:
+    string = input("Enter a word: ")
+    if string == "chupacabra":
+        break
+print("You've successfully left the loop.")
---
+++ pe1-module3/test_program.py
@@ -0,0 +1,51 @@
+# counter = 6
+# while counter:
+#     print("Inside the loop.", counter)
+#     counter -= 1
+# print("Outside the loop.", counter)
+
+# for i in range(2, 8):
+#     print("The value of i is currently", i)
+
+# for i in range(1, 1):
+#     print("The value of i is currently", i)
+
+# power = 1
+# for expo in range(16):
+#     print("2 to the power of", expo, "is", power)
+#     power *= 2
+# largest_number = -99999999
+# counter = 0
+
+# while True:
+#     number = int(input("Enter a number or type -1 to end the program: "))
+#     if number == -1:
+#         break
+#     counter += 1
+#     if number > largest_number:
+#         largest_number = number
+
+# if counter != 0:
+#     print("The largest number is", largest_number)
+# else:
+#     print("You haven't entered any number.")
+
+largest_number = -99999999
+counter = 0
+
+number = int(input("Enter a number or type -1 to end program: "))
+
+while number != -1:
+    if number == -1:
+        continue
+    counter += 1
+
+    if number > largest_number:
+        largest_number = number
+    number = int(input("Enter a number or type -1 to end the program: "))
+
+if counter:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
+
