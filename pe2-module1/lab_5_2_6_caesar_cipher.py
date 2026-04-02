077d062cbe2389c843fdb1bbf51652d3ce81890f
Samwel Simule <s098153@student.tu.kielce.pl>
2026-04-02 21:08:54 +0200

    Refactor Caesar cipher implementation for improved readability and functionality

--- pe2-module1/lab_5_2_6_caesar_cipher.py
+++ pe2-module1/lab_5_2_6_caesar_cipher.py
@@ -1,4 +1,5 @@
-# Caesar cipher.
+# caesar cipher 
+# A Caesar cipher is a simple encryption technique where each letter in the plaintext is shifted a certain number of places down the alphabet.
 text = input("Enter your message: ")
 cipher = ''
 shift_number = int(input("Enter shift number: "))
@@ -10,18 +11,21 @@ if shift_number < 1:
 
 for char in text:
     if not char.isalpha():
-        continue
+         cipher += char
+    
     if char.isupper():
         code = ord(char) + shift_number
-        if code == ord('Z'):
-            code = ord('A') + shift_number - 1
+
+        if code > ord('Z'):
+            code = ord(char) + shift_number -  (ord('Z') - ord('A') + 1)
 
         cipher += chr(code)
+
     if char.islower():
         code = ord(char) + shift_number
-        if code == ord('z'):
-            code = ord('a') + shift_number - 1
 
+        if code > ord('z'):
+            code = ord(char) + shift_number - (ord('z') - ord('a') + 1)
         cipher += chr(code)
 
 print(cipher)
