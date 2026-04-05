8373e9b35c9090f285e0e484842ceb8397d56a76
Samwel Simule <s098153@student.tu.kielce.pl>
2026-04-05 19:04:03 +0200

    Comment out unused variables and loops in test_program.py for clarity

--- pe1-module3/test_program.py
+++ pe1-module3/test_program.py
@@ -30,22 +30,28 @@
 # else:
 #     print("You haven't entered any number.")
 
-largest_number = -99999999
-counter = 0
+# largest_number = -99999999
+# counter = 0
 
-number = int(input("Enter a number or type -1 to end program: "))
+# number = int(input("Enter a number or type -1 to end program: "))
 
-while number != -1:
-    if number == -1:
-        continue
-    counter += 1
+# while number != -1:
+#     if number == -1:
+#         continue
+#     counter += 1
 
-    if number > largest_number:
-        largest_number = number
-    number = int(input("Enter a number or type -1 to end the program: "))
+#     if number > largest_number:
+#         largest_number = number
+#     number = int(input("Enter a number or type -1 to end the program: "))
 
-if counter:
-    print("The largest number is", largest_number)
-else:
-    print("You haven't entered any number.")
+# if counter:
+#     print("The largest number is", largest_number)
+# else:
+#     print("You haven't entered any number.")
 
+# i = 1
+# while True:
+#     if i == 3:
+#         break
+#     print(i, end=" ")
+#     i += 1
