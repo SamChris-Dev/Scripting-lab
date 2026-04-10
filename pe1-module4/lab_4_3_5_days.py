08235780e76c29319fd2f30c690d742effd163d9
Samwel Simule <s098153@student.tu.kielce.pl>
2026-04-10 22:27:39 +0200

    Refactor leap year logic and consolidate month days calculation; remove redundant file

--- pe1-module4/lab_4_3_5_days.py
+++ pe1-module4/lab_4_3_5_days.py
@@ -1,17 +1,29 @@
-
 def is_year_leap(year):
-    if year % 4 != 0:
-        return False
-    elif year % 100 != 0:
+    if year % 4 == 0 and year % 100 != 0:
         return True
-    elif year % 400 != 0:
-        return False
+    elif year % 400 == 0:
+        return True
     else:
-        return True
-def days_in_month(year, month):
-    if month < 1 or month > 12:
         return False
 
+def days_in_month(year, month):
+    if is_year_leap(year) == False:
+        if month <= 7 and month % 2 != 0 \
+        or month >= 8 and month % 2 ==0:
+            return 31
+        elif month == 2:
+            return 28
+        else:
+            return 30
+    elif is_year_leap(year) == True:
+        if month <= 7 and month % 2 != 0 \
+        or month >= 8 and month % 2 ==0:
+            return 31
+        elif month == 2:
+            return 29
+        else:
+            return 30
+    
 test_years = [1900, 2000, 2016, 1987]
 test_months = [2, 2, 1, 11]
 test_results = [28, 29, 31, 30]
@@ -24,3 +36,8 @@ for i in range(len(test_years)):
         print("OK")
     else:
         print("Failed")
+
+# the_year = int(input("Enter year: "))
+# the_month = int(input("Enter Month: "))
+
+# print(days_in_month(the_year,the_month))
