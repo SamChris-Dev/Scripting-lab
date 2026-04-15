f234cbd673533825487bc76dc6166754b12214da
Samwel Simule <s098153@student.tu.kielce.pl>
2026-04-15 03:35:15 +0200

    Add student score tracking and dictionary iteration examples

---
+++ pe1-module3/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+for key in dictionary.values():
+#     print(key, "->", value)
--- pe1-module3/test_program.py
+++ pe1-module3/test_program.py
@@ -308,3 +308,42 @@
 
 # t10, t20, t30 = t30, t10, t20
 # print(t10, t20, t30)
+
+
+# dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
+
+# for key in dictionary.keys():
+#     print(key, "->", dictionary[key])
+
+# for key in dictionary.values():
+#     print(key, "->", values)
+
+# print("\n")
+# for key, item in dictionary.items():
+#     print(key, "->", item)
+
+
+# school_class = {}
+
+# while True:
+#     name = input("Enter the student's name: ")
+#     if name == '':
+#         break
+    
+#     score = int(input("Enter the student's score (0-10): "))
+#     if score not in range(0, 11):
+#         break
+
+#     if name in school_class:
+#         school_class[name] += (score,)
+#     else:
+#         school_class[name] = (score,)
+        
+# for name in sorted(school_class.keys()):
+#     adding = 0
+#     counter = 0
+#     for score in school_class[name]:
+#         adding += score
+#         counter += 1
+#     print(name, ":", adding / counter)
+
