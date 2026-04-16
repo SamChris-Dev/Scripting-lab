533f5cfc802c9f2c7c34f6e700d46d0a9763fc12
Samwel Simule <s098153@student.tu.kielce.pl>
2026-04-16 10:22:09 +0200

    Refactor Tic-Tac-Toe game logic by improving computer move input handling and enhancing user move display

--- pe2-project-Tic-Tac-Toe/tic_tac_toe.py
+++ pe2-project-Tic-Tac-Toe/tic_tac_toe.py
@@ -2,29 +2,62 @@ from random import randrange
 
 
 
-board = [[1,2,3],[4,'x',6],[7,8,9]]
+board = [
+    [1,2,3],
+    [4,'X',6],
+    [7,8,9]
+    ]
 loop_on = True
 
 def comp_mv(board):
-    comp_move = 2
+    comp_move = int(input("Enter computer move: "))
 
     if comp_move == 2:
         board[0][1] = "X"
-        print(board)
+        print("\n")
+
+        for row in board:
+            for item in row:
+                print("|",item, end=" | ")
+            print()
+    
+    if comp_move == 8:
+        board[2][1] = "X"
+        print("\n")
+
+        for row in board:
+            for item in row:
+                print("|",item, end=" | ")
+            print()
+    
+
 
 
 
 def usr_mv(board):
     user_move = int(input("Enter a number between 1 and 9: "))
 
+    
     if user_move == 1:
         board[0][0] = "O"
-        print(board)
+        for row in board:
+            for item in row:
+                print("|",item, end=" | ")
+            print()
+
+    if user_move == 4:
+        board[1][0] = "O"
+        for row in board:
+            for item in row:
+                print("|",item, end=" | ")
+            print()
 
 
-def winner(n):
-    if n == 2:
+def winner():
+    if board[0][1] == board[1][1] == board[2][1]:
+        print("The computer won")
         return 0
+    
 
 def check_space():
     pass
@@ -34,8 +67,11 @@ def check_space():
 while loop_on:
     usr_mv(board)
     comp_mv(board)
-    winner(1)
+    winner()
+
+    x = winner()
+    print(x)
     
-    if winner(1) == 0:
+    if winner() == 0:
         break
 
