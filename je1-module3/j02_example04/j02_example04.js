dccd9bf017b08acf9436322bd5488fe8f744984a
SamChris <s098153@student.tu.kielce.pl>
2026-04-30 08:44:50 +0200

    created three javascript directory

---
+++ je1-module3/j02_example04/j02_example04.js
@@ -0,0 +1,10 @@
+
+function addStudent() {
+    const name = prompt("Enter student name:");
+
+    if (name) {
+        const listItem = document.createElement("li");
+        listItem.textContent = name;
+        studentList.appendChild(listItem);
+    }
+}
