c944dc9e8bff90224b68dbba775bac18799161eb
Samwel Simule <s098153@student.tu.kielce.pl>
2026-05-03 23:19:10 +0200

    Refactored import statement and update function calls for clarity

--- pe2-module1/quadratic_solver.py
+++ pe2-module1/quadratic_solver.py
@@ -1,13 +1,13 @@
 # This program is a quadratic equation solver that calculates the roots of a quadratic equation given the coefficients a, b, and c.
 
-import math as calc
+import math
 
 a = int(input("Enter the coefficient of a : "))
 b = int(input("Enter the coefficient of b : "))
 c = int(input("Enter the coefficient of c : "))
 
-x1 = (-b + calc.sqrt((b**2) - (4 * a * c)))/(2 * a)
-x2 = (-b - calc.sqrt((b**2) - (4 * a * c)))/(2 * a)
+x1 = (-b + math.sqrt((b**2) - (4 * a * c)))/(2 * a)
+x2 = (-b - math.sqrt((b**2) - (4 * a * c)))/(2 * a)
 
 print("The value of x1 and x2 from the equation (",a,"x² + ",b,"x + ",c,") is: ", sep= "")
 print("x1: ", x1)
