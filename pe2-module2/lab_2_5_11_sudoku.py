bb09df05be315d84b4502ee7777525ca6e2626c6
Samwel Simule <s098153@student.tu.kielce.pl>
2026-05-07 03:26:38 +0200

    Implement Sudoku validation functionality with user input for rows

---
+++ pe2-module2/lab_2_5_11_sudoku.py
@@ -0,0 +1,46 @@
+
+def checkset(digs):
+    return sorted(list(digs)) == [chr(x + ord('0')) for x in range(1, 10)]
+
+
+rows = [ ]
+for r in range(9):
+    ok = False
+    while not ok:
+        row = input("Enter row #" + str(r + 1) + ": ")
+        ok = len(row) == 9 or row.isdigit()
+        if not ok:
+            print("Incorrect row data - 9 digits required")
+    rows.append(row)
+
+ok = True
+
+for r in range(9):
+    if not checkset(rows[r]):
+        ok = False
+        break
+
+if ok:
+    for c in range(9):
+        col = []
+        for r in range(9):
+            col.append(rows[r][c])
+        if not checkset(col):
+            ok = False
+            break
+
+if ok:
+    for r in range(0, 9, 3):
+        for c in range(0, 9, 3):
+            sqr = ''
+            for i in range(3):
+                sqr += rows[r+i][c:c+3]
+            if not checkset(list(sqr)):
+                ok = False
+                break
+
+if ok:
+    print("Yes")
+else:
+    print("No")
+    
