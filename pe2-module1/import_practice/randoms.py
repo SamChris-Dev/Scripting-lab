ac94f8ba81118ad0b6e778766fb4175eb62bd0fe
Samwel Simule <s098153@student.tu.kielce.pl>
2026-05-11 23:30:26 +0200

    Add .gitignore, VSCode settings, and initial requirements file; create randoms.py for platform info

---
+++ pe2-module1/import_practice/randoms.py
@@ -0,0 +1,15 @@
+# import random
+
+# random.seed(0)
+
+
+# for i in range(5):
+#     print(random.random())
+
+import platform
+
+print(platform.platform())
+print(platform.machine())
+print(platform.processor())
+print(platform.system())
+print(platform.version())
